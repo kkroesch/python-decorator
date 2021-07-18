1ea8aaea5fc04ce033b8c3c0fe12b80a5bde8977
Karsten Kroesch <karsten.kroesch@ispin.ch>
2021-07-18 20:19:37 +0200

    Initial impoort

--- tracer.py
+++ tracer.py
@@ -33,10 +33,10 @@ def timer(func):
     """Print the runtime of the decorated function"""
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = perf_counter()    # 1
+        start_time = perf_counter()
         value = func(*args, **kwargs)
-        end_time = perf_counter()      # 2
-        run_time = end_time - start_time    # 3
+        end_time = perf_counter()
+        run_time = end_time - start_time
         debug(f"Finished {func.__name__!r} in {run_time:.4f} secs")
         return value
     return wrapper_timer
